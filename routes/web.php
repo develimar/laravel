ac087c0f707d2b2f8cbc0e66304afbca4c77693b
Prime_Midia <elimar.pc@gmail.com>
2022-05-04 11:35:40 -0300

    Enviando paramentros nas rotas

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,10 @@ Route::get('/', function () {
 Route::get('/', 'PrincipalController@Principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@Contato');
+// nome, categoria, assunto, mensagem
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem){
+   echo 'Estamos aqui '.$nome.$categoria.$assunto.$mensagem;
+});
 
 
