dfa09d57a8a83669e8921b1d53a48063fb77dffe
Prime_Midia <elimar.pc@gmail.com>
2022-05-04 17:04:26 -0300

    Inserção de comentario no blade e codigo puro php dentro do blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+
+
+{{-- Comentario em blade e é descartado --}}
+
+{{-- Inserção de codificação pura do php dentro do blade template--}}
+@php
+    echo 'Texto de teste';
+@endphp
+
