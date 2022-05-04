d4ab9c3e289d99b86c86f18cca46be25f4c27c85
Prime_Midia <elimar.pc@gmail.com>
2022-05-04 12:00:37 -0300

    Criando menu de Navegação

--- routes/web.php
+++ routes/web.php
@@ -31,18 +31,17 @@ Route::get('/', function () {
 });
 */
 
+//Route::get('/contato/{nome}/{categoria_id}',
+//    function(
+//        string $nome = 'Elimar',
+//        int $categoria_id = 1 // 1 - 'Informação'
+//    ){
+//   echo 'Estamos aqui '.$nome.' - '.$categoria_id;
+//})->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+
 
 Route::get('/', 'PrincipalController@Principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@Contato');
-// nome, categoria, assunto, mensagem
-
-Route::get('/contato/{nome}/{categoria_id}',
-    function(
-        string $nome = 'Elimar',
-        int $categoria_id = 1 // 1 - 'Informação'
-    ){
-   echo 'Estamos aqui '.$nome.' - '.$categoria_id;
-})->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
 
 
