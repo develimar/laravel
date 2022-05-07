ad4c5a780d85f0ea1e714487f6e5a0e382bc00f2
develimar <elimar.pc@gmail.com>
2022-05-07 12:52:31 -0300

    For no blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,29 +2,35 @@
 
 {{--@dd($fornecedores)--}}
 
-@isset($fornecedores)
-    Fornecedor:{{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido.'}}
-    <br>
-    Telefone: ({{ $fornecedores[0]['ddd'] ?? ''}}){{ $fornecedores[0]['telefone'] ?? ''}}
-    <br>
-    Cidade:
-    @switch($fornecedores[0]['ddd'])
-        @case ('11')
-            São paulo
-            @break
-        @case ('38')
-            Curvelo
-            @break
-        @case ('31')
-            Sete Lagoas
-            @break
-        @default
-            Estado não identificado.
-    @endswitch
-@endisset
+    @isset($fornecedores)
+
+        @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor:{{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido.'}}
+        <br>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? ''}}){{ $fornecedores[$i]['telefone'] ?? ''}}
+        <br>
+        Cidade:
+        @switch($fornecedores[$i]['ddd'])
+            @case ('11')
+                São paulo
+                @break
+            @case ('38')
+                Curvelo
+                @break
+            @case ('31')
+                Sete Lagoas
+                @break
+            @default
+                Estado não identificado.
+        @endswitch
+        <hr>
+
+        @endfor
+
+    @endisset
 
 
