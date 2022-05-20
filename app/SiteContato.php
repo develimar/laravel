808ea316adea6dc1ae59928dd3cf9c346ec07e70
Prime_Midia <elimar.pc@gmail.com>
2022-05-20 15:33:11 -0300

    Inserindo registro em tabelas de nomes diferente pelo tinker

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//Site_Contato -> todas são mudadas para minusculo -> depois é colocado um s no final
+
 class SiteContato extends Model
 {
     //
