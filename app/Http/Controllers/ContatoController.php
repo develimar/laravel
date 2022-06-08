6fe60b67638b4538bbd60746e09868ebd5d8263b
Prime_Midia <elimar.pc@gmail.com>
2022-06-08 16:13:58 -0300

    Gravando dados vindo de formularios no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
         $parametros = [
             'titulo' => 'Contato',
         ];
 
-        var_dump($_POST);
+        //$contato = new SiteContato();
+//        $contato->nome = $request['nome'];
+//        $contato->telefone = $request['telefone'];
+//        $contato->email = $request['email'];
+//        $contato->motivo_contato = $request['motivo_contato'];
+//        $contato->mensagem = $request->input('mensagem');
+//        $contato->save();
+
+        $contato = new SiteContato();
+
+        //$contato->fill($request->all())->save();
+        $contato->create($request->all());
+
+        //$contato->save();
+
         return view('site.contato', $parametros);
     }
 }
